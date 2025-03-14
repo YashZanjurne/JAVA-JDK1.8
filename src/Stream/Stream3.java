6a1b3fb2e6c687a3a2e26d17615636388b6328a6
hp <zanjurneyash2002@gmail.com>
2025-03-14 14:37:19 +0530

    Even Numbers of list

---
+++ src/Stream/Stream3.java
@@ -0,0 +1,16 @@
+package Stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream3 {
+    public static void main(String[] args) {
+        List<Integer>list = List.of(1,2,3,4,5,6,7,8,9,10);
+
+       List<Integer>listEven =list.stream().filter(i-> i%2==0).collect(Collectors.toList());
+        System.out.println(listEven);
+
+      List<Integer>list1 =   listEven.stream().map(i->i*2).collect(Collectors.toList());
+        System.out.println(list1);
+    }
+}
